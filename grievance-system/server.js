bda7ff1cf8cd46d39e515ec1a79779201bb7ca14
anmolsingh7275 <anmolprogramming@gmail.com>
2024-09-11 22:37:13 +0530

    env and frontent setup

--- grievance-system/server.js
+++ grievance-system/server.js
@@ -15,6 +15,6 @@ app.use('/api/complaints', complaintRoutes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
